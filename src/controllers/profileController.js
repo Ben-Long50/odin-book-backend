dd8915b661c196baf6ef229228598eb0e03571d8
Ben-Long50 <benjlong50@gmail.com>
2024-10-05 11:06:55 -0700

    fixed error handling on profile creation

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -14,21 +14,36 @@ const profileController = {
     }
   },
 
-  createOrUpdateProfile: async (req, res) => {
-    try {
-      const profile = await profileServices.createOrUpdateProfile(
-        req.body,
-        req.user.id,
-      );
-      if (profile && req.body.id) {
-        res.status(200).json({ message: 'Successfully updated profile' });
+  createOrUpdateProfile: [
+    body('username', 'Username must be a minimum of 2 characters')
+      .trim()
+      .isLength({ min: 2 })
+      .escape(),
+    body('petName', 'Pet name must be a minimum of 2 characters')
+      .trim()
+      .isLength({ min: 2 })
+      .escape(),
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.status(400).json(errors.array());
       } else {
-        res.status(200).json({ message: 'Successfully created profile' });
+        try {
+          const profile = await profileServices.createOrUpdateProfile(
+            req.body,
+            req.user.id,
+          );
+          if (profile && req.body.id) {
+            res.status(200).json({ message: 'Successfully updated profile' });
+          } else {
+            res.status(200).json({ message: 'Successfully created profile' });
+          }
+        } catch (error) {
+          res.status(500).json({ message: error.message });
+        }
       }
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  },
+    },
+  ],
 
   setActiveProfile: async (req, res) => {
     try {
