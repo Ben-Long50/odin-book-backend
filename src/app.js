c0e5a5562dc905031a1396bd8b549d616dfb7d90
Ben-Long50 <benjlong50@gmail.com>
2024-11-05 17:01:34 -0800

    adjustments to global error handler middleware function

--- src/app.js
+++ src/app.js
@@ -91,13 +91,15 @@ app.use((req, res, next) => {
 
 // error handler
 app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // Send JSON error response
-  res.status(err.status || 500);
-  res.json({
+  res.status(err.status || 500).json({
     message: err.message,
     error: req.app.get('env') === 'development' ? err : {},
   });
